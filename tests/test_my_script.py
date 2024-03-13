bcbfde5ebd0a40a4115d64e962c706a1d051319f
christian ochenehi peter <christianochenehipeter@gmail.com>
2024-03-13 10:01:49 +0100

    test

--- tests/test_my_script.py
+++ tests/test_my_script.py
@@ -1,5 +1,5 @@
 # tests/test_my_script.py
-from .. import my_script  # Use two dots to go up one level
+from  ..import my_script  # Use two dots to go up one level
 
 def test_add_numbers():
     assert my_script.add_numbers(2, 3) == 5
